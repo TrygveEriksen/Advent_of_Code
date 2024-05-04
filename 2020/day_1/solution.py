6e1d4ef21005dcd10d545b2615f75bf406d566a9
Trygve Eriksen <tryger@stud.ntnu.no>
2024-05-04 21:12:30 +0200

    complete 2020 day 1

---
+++ 2020/day_1/solution.py
@@ -0,0 +1,34 @@
+import time
+
+def getNums() -> set: 
+    with open("2020/day_1/input.txt", "r") as f: 
+        nums = {int(i) for i in f.readlines()}
+    return nums
+
+
+def part_1() -> int:
+    nums = getNums()
+    for num in nums: 
+        if 2020 - num in nums: 
+            return (2020-num)*num
+    return -1 #found nothing
+
+
+def part_2() -> int:  
+    nums = getNums()
+    for num1 in nums: 
+        for num2 in nums: 
+            if 2020 - (num1 + num2) in nums: 
+                return (2020 - (num1 + num2)) * num1 * num2
+    return -1 #found nothing
+
+     
+
+    
+def main():
+    start = time.perf_counter()
+    print(part_2())
+    stop = time.perf_counter()
+    print(f"Process used {stop-start} seconds")
+
+main()
